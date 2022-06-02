f342412afa0b701bdd0ebaf3ac1ddf3922964995
hbadejo <badejohakeem95@gmail.com>
2022-06-02 14:53:08 -0700

    Final commit for 0x00

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 	printf("Size of a char: %d byte(s)\n", (signed int)sizeof(c));
 	printf("Size of an int: %d byte(s)\n", (signed int)sizeof(i));
 	printf("Size of a long int: %d byte(s)\n", (signed int)sizeof(li));
-	printf("Size of a long long int: %d byte(s) \n", (signed int)sizeof(lli));
+	printf("Size of a long long int: %d byte(s)\n", (signed int)sizeof(lli));
 	printf("Size of a float: %d byte(s)\n", (signed int)sizeof(f));
 	return (0);
 }
