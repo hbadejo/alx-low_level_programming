703fdbfe52b778382931fd277ac59552b2f477f0
hbadejo <badejohakeem95@gmail.com>
2022-06-07 16:49:00 -0700

    101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print natural number below 1024 divisible by 5 and 3
+ *
+ * Return: Return print
+ */
+int main(void)
+{
+	int i, j = 0;
+
+	for (i = 0; i < 1024; i++)
+		if (i % 3 == 0 || i % 5 == 0)
+			j += i;
+	printf(j);
+	return (0);
+}
