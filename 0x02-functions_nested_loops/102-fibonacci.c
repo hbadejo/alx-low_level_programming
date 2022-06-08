6fb2356665ad0072970dda3248e4e19d07439b8a
hbadejo <badejohakeem95@gmail.com>
2022-06-07 17:38:03 -0700

    102 Advance

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - 1st to 50th term of Fibonacci Sequence
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int _, _i, _j, _r;
+
+	_i = 1;
+	_j = 2;
+
+	printf("%d, %d\n", _i, _j);
+
+	for (_ = 0; _ < 48; _++)
+	{
+		r = _i + _j;
+		if (_ == 47)
+			printf("%d\n", _r);
+		else
+			printf("%d, ", _r);
+
+		_i = _j;
+		_j = _r;
+	}
+	return (0);
+}
