f90de172857f6e1257bb6caf6bac16d61e1333bc
hbadejo <badejohakeem95@gmail.com>
2022-06-09 11:45:11 -0700

    update to q1 debuging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,10 +5,18 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
+	//int i;
 
 	printf("Infinite loop incoming :(\n");
+
+	/*i = 0;
+	while (i < 0)
+	{
+		putchar(i);
+	}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
