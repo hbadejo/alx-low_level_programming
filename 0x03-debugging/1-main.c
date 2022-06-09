db0946232e15bcdf078f312b6859d18e630ff72c
Abdulhakeem BADEJO <32986101+hbadejo@users.noreply.github.com>
2022-06-09 14:59:00 +0100

    Add files via upload
    Commit for debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	printf("Infinite loop incoming :(\n");
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @_: first int
+ * @_i: second int
+ * @_j: third int
+ * Return: Largest number
+ */
+int largest_number(int _, int _i, int _j)
+{
+	int largest;
+
+	if (_ > _i)
+	{
+		if (_ > _j)
+			largest = _;
+		else
+			largest = _j;
+	}
+	else if (_i > _j)
+		largest = _i;
+	else
+		largest = _j;
+
+	return (largest);
+}
