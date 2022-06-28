2ce9c2b367b50047b527bafbb01e6da0d9a45ea3
hbadejo <badejohakeem95@gmail.com>
2022-06-28 14:05:04 -0700

    update to strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,17 +8,13 @@
  *
  * Return:  On success,
  * the _strdup() function returns a pointer to the duplicated
- * string.  It returns NULL if insufficient memory was availab
+ * string.  It returns NULL if insufficient memory was allocated
 */
-
-
 char *_strdup(char *str)
 {
 	char *dup;
-	unsigned int i, lenght;
+	unsigned int i = 0, lenght = 0;
 
-	i = 0;
-	lenght = 0;
 	if (str == NULL)
 	{
 		return (NULL);
