c327795b297844aab098ef2fd7c5f0d829c9b591
hbadejo <badejohakeem95@gmail.com>
2022-07-04 14:56:44 -0700

    First push for macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define NAME __FILE__
+
+/**
+ * main - Print source code file name
+ *
+ * Return: Return 0
+ */
+int main(void)
+{
+	printf("%s\n", NAME);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+This is about Macro and Structure
